9579372be123fe3f6a4e126299eda3fa7220e1d3
stjuilletc <stjuilletc@gmail.com>
2017-07-03 08:58:23 +0200

    d

---
+++ src/Test/Test.java
@@ -0,0 +1,12 @@
+package Test;
+
+/**
+ * Created by tom on 03/07/2017.
+ */
+public class Test {
+
+    public static void main(String [] args ){
+        int a=2;
+        System.out.println(a);
+    }
+}
