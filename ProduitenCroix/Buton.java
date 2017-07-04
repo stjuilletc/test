fe3b1e0621eade800f78bcc47b0a6fd462516875
stjuilletc <stjuilletc@gmail.com>
2017-07-04 15:08:24 +0200

    ProduitEnCroix

---
+++ ProduitenCroix/Buton.java
@@ -0,0 +1,34 @@
+package ProduitenCroix;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Buton extends JButton implements ActionListener {
+
+    Tableau f;
+    String s;
+
+    public Buton(Tableau f, String s) {
+        super(s);
+        this.f = f;
+        this.s = s;
+        this.addActionListener(this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (s.equals("Reset")) {
+            f.reset();
+        }
+        if (s.equals("OK")) {
+            f.calcul();
+        }
+    }
+
+    public static class Main {
+        public static void main(String [] args){
+            Tableau f = new Tableau();
+        }
+    }
+}
